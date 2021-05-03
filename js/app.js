869aa4cce098793cf673a7e0e6278a340ac193b4
Yacine-sinapayen <yacine.sinapayen@hotmail.fr>
2021-05-03 08:49:36 +0200

    project initialization

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// ton code ici
+
+// Lobjectif est de générer un grille de 8cases*8
+//on va compter de 0 à 7 dans un v-bloucle for 
+    // générer 1 div avec la class row
+
+    //Puis pour générer 1  div avec la class cell
